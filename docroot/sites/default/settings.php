fc6bf8020ab6b4633ae47d9488ab3978017bf242
Adam Balsam <adam.balsam@acquia.com>
2018-02-27 02:16:04 -0500

    reasonable

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -779,10 +779,11 @@ $settings['entity_update_batch_size'] = 50;
  *
  * Keep this code block at the end of this file to take full effect.
  */
-#
-# if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
-#   include $app_root . '/' . $site_path . '/settings.local.php';
-# }
+
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+  include $app_root . '/' . $site_path . '/settings.local.php';
+}
+
 $config_directories['sync'] = '../config/sync';
 
 if (file_exists('/var/www/site-php')) {
