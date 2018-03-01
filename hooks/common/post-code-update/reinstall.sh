7c34d6516a1d464b17d0f81500d6361ad5c8e3aa
Adam Balsam <adam.balsam@acquia.com>
2018-02-28 22:50:05 -0500

    Update cloud hook verbs

--- hooks/common/post-code-update/reinstall.sh
+++ hooks/common/post-code-update/reinstall.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 #
-# Cloud Hook: Reinstall Lightning
-#
-# Install? That doesn't sound sustainable. How about just an import.
+# Cloud Hook: Import config
 
 site="$1"
 target_env="$2"
 
-# Fresh install of Lightning.
 drush @$site.$target_env cim --yes
