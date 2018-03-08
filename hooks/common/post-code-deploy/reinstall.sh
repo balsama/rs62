4f9c0d427953114fbca09ddfff6302ce26afbf6e
Adam Balsam <balsama@users.noreply.github.com>
2018-03-07 21:49:23 -0500

    Update to 8.5.0 (#11)

--- hooks/common/post-code-deploy/reinstall.sh
+++ hooks/common/post-code-deploy/reinstall.sh
@@ -5,6 +5,8 @@
 site="$1"
 target_env="$2"
 
+drush @$site.$target_env cr
+drush @$site.$target_env updatedb --yes
 drush @$site.$target_env cim sync --yes
 drush @$site.$target_env cr
 
--- hooks/common/post-code-update/reinstall.sh
+++ hooks/common/post-code-update/reinstall.sh
@@ -5,5 +5,7 @@
 site="$1"
 target_env="$2"
 
+drush @$site.$target_env cr
+drush @$site.$target_env updatedb --yes
 drush @$site.$target_env cim sync --yes
 drush @$site.$target_env cr
