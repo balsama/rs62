c69fc11fa5d13dc6827078b35d28b3e9e8ad11fb
Adam Balsam <adam.balsam@acquia.com>
2019-06-22 22:09:08 -0400

    Add jamchart

--- docroot/modules/custom/jamchart/src/Controller/AssetController.php
+++ docroot/modules/custom/jamchart/src/Controller/AssetController.php
@@ -2,39 +2,54 @@
 
 namespace Drupal\jamchart\Controller;
 
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\dynamic_asset\Controller\DynamicAssetBaseController;
+use Drupal\jamchart\Request;
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class AssetController extends DynamicAssetBaseController {
+class AssetController extends DynamicAssetBaseController implements ContainerInjectionInterface {
+
+  /**
+   * @var \GuzzleHttp\Client
+   */
+  protected $client;
+
+  /**
+   * @var Request
+   */
+  protected $request;
+
+  public function __construct(ClientInterface $client) {
+    $this->client = $client;
+    $this->request = new Request(\Drupal::request()->getSchemeAndHttpHost() . base_path(), $this->client);
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('http_client')
+    );
+  }
 
   protected function getJS() {
-    return '
-            google.charts.load("current", {packages:["calendar"]});
-                google.charts.setOnLoadCallback(drawChart);
-        
-                function drawChart() {
-                    var dataTable = new google.visualization.DataTable();
-                    dataTable.addColumn({ type: \'date\', id: \'Date\' });
-                    dataTable.addColumn({ type: \'number\', id: \'Won/Loss\' });
-                    dataTable.addRows([
-                        [ new Date(2012, 3, 12), 1 ],
-                        [ new Date(2012, 4, 11), 1 ],
-                        [ new Date(2012, 5, 9), 1 ],
-                        [ new Date(2012, 6, 8), 1 ],
-                        [ new Date(2012, 7, 22), 1 ]
-                        // Many rows omitted for brevity.
-        
-                    ]);
-        
-                    var chart = new google.visualization.Calendar(document.getElementById(\'calendar_basic\'));
-        
-                    var options = {
-                        title: "Jam frequency",
-                        height: 350,
-                    };
-        
-                    chart.draw(dataTable, options);
-                }
-            ';
+    $sessions_info = $this->getSessionInfo();
+    $sessions_processed = [];
+    foreach ($sessions_info as $session_info) {
+      $sessions_processed[] = $this->process_session_info($session_info);
+    }
+    return $this->injectDates($sessions_processed);
+  }
+
+  protected function process_session_info(\stdClass $session_info) {
+    $date_parts = explode('-', $session_info->attributes->field_session_date);
+    $musician_count = count($session_info->relationships->field_musicians->data);
+    $month = $date_parts[1] - 1;
+    return "[ new Date($date_parts[0], $month, $date_parts[2]), $musician_count ]";
+  }
+
+  protected function getSessionInfo() {
+    $request = $this->request->request('jsonapi/node/session?fields[node--session]=field_session_date,field_musicians');
+    return $request->data;
   }
 
   /**
@@ -44,4 +59,45 @@ class AssetController extends DynamicAssetBaseController {
     return $this->js($this->getJS());
   }
 
+  protected function injectDates($sessions_processed) {
+    return '
+            google.charts.load("current", {packages:["calendar"]});
+                google.charts.setOnLoadCallback(drawChart);
+
+                function drawChart() {
+                    var dataTable = new google.visualization.DataTable();
+                    dataTable.addColumn({ type: "date", id: "Date" });
+                    dataTable.addColumn({ type: "number", id: "Number of musicians" });
+                    dataTable.addRows(['
+                      . implode(',', $sessions_processed) .
+                    ']);
+
+                    var chart = new google.visualization.Calendar(document.getElementById("calendar_basic"));
+
+                    var options = {
+                        title: "Session dates with number of musicians",
+                        height: 500,
+                        calendar: {
+                          monthOutlineColor: {
+                            stroke: "gray",
+                            strokeOpacity: 0.8,
+                            strokeWidth: 1
+                          },
+                          focusedCellColor: {
+                            stroke: "#9e9e9e",
+                            strokeOpacity: 1,
+                            strokeWidth: 2
+                          }
+                        },
+                        colorAxis: {colors:["#ffccbc", "#ff7043"]},
+                        noDataPattern: {
+                          backgroundColor: "#f5f5f5",
+                          color: "#f5f5f5"
+                        }
+                    };
+
+                    chart.draw(dataTable, options);
+                }
+            ';
+  }
 }
--- docroot/modules/custom/jamchart/src/Controller/JamchartController.php
+++ docroot/modules/custom/jamchart/src/Controller/JamchartController.php
@@ -17,7 +17,7 @@ class JamchartController extends ControllerBase {
       "#attached" => [
         'library' => 'jamchart/jamchart'
       ],
-      "#markup" => "<div id='calendar_basic'>chart here</div>"
+      "#markup" => "<div class='col 12'><div id='calendar_basic'><p>Please wait... rendering the Jam Chart</p></div><p class='col m8'>Days that a session was recorded are highlighted in orange with varying degrees of intensity based on the number of musicians at the jam.</p></div>"
     ];
     return $element;
   }
